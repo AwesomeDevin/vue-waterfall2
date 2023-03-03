300505f44980bbbc215f393da89f526e1a8d2d51
awesomedevin <awesomedevin@163.com>
2023-03-03 22:12:47 +0800

    docs: update

--- config/index.js
+++ config/index.js
@@ -38,7 +38,7 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, './index.html'),
+    index: path.resolve(__dirname, '../docs/index.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
